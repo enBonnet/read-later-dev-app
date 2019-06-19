e312813755c41554e6a218e93acef4e0bdebe21f
Ender Bonnet <ebonnet@eclass.cl>
2019-06-19 17:15:35 -0400

    change export method on actions

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,4 @@
-exports.togglePostToRead = (state, post, dispatch) => {
+export async function togglePostToRead(state, post, dispatch) {
   if (state.toReadPosts.includes(post)) {
     const postRemoved = state.toReadPosts.filter(
       toRead => toRead.id !== post.id
@@ -7,12 +7,12 @@ exports.togglePostToRead = (state, post, dispatch) => {
   }
 
   return dispatch({ type: "ADD_TOREAD", payload: post });
-};
+}
 
-exports.fetchPosts = async (dispatch, page = 1, tag = "react") => {
+export async function fetchPosts(dispatch, page = 1, tag = "react") {
   const data = await fetch(
     `https://dev.to/api/articles?page=${page}&tag=${tag}`
   );
   const dataJSON = await data.json();
   dispatch({ type: "FETCH_POSTS", payload: dataJSON });
-};
+}
